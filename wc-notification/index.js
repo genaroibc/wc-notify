27d596f707534ee72743668f4312fc9382b4311a
genaroibc <bonavitaismael@gmail.com>
2023-02-11 20:07:37 -0300

    [REFACTOR]: re-implement new notification functionality

--- wc-notification/index.js
+++ wc-notification/index.js
@@ -2,11 +2,6 @@ import { defineWCNotification } from "./wc-notification.js";
 
 defineWCNotification();
 
-const NOTIFICATION_ITEM_HEIGHT = 80;
-const config = {
-  nOfNotifications: 0
-};
-
 function uuid() {
   return String(Math.random() * Math.random());
 }
@@ -14,25 +9,15 @@ function uuid() {
 function createNotificationItem(message) {
   const item = document.createElement("span");
   const itemId = uuid(); // add a random uuid
-  const notificationContainer = document.querySelector("wc-notification");
-  const notificationItems = Array.from(
-    notificationContainer.querySelectorAll("[data-wc-notification-item-id]")
+  const notificationContainer = document.querySelector(
+    "wc-notification .wc-notification-container"
   );
 
-  notificationItems.reverse().forEach((item, index) => {
-    item.style = `transform: translateY(${
-      NOTIFICATION_ITEM_HEIGHT * (index + 1)
-    }px)`;
-  });
-
-  item.setAttribute("style", `translate: -50% 0`);
   item.setAttribute("data-wc-notification-item-id", itemId);
   item.classList.add("wc-notification-item", "wc-notification-item-visible");
   item.textContent = message;
 
   notificationContainer.appendChild(item);
-
-  config.nOfNotifications++;
 
   setTimeout(() => {
     const notificationItems = Array.from(
@@ -46,8 +31,6 @@ function createNotificationItem(message) {
     if (notificationItemToRemove) {
       notificationContainer.removeChild(notificationItemToRemove);
     }
-
-    config.nOfNotifications--;
   }, 4000);
 }
 
--- wc-notification/wc-notification.js
+++ wc-notification/wc-notification.js
@@ -7,27 +7,43 @@ class WCNotification extends HTMLElement {
 
   connectedCallback() {
     this.innerHTML = `<style>
-wc-notification .wc-notification-item {
-  max-width: 100vw;
-  display: block;
+wc-notification .wc-notification-container {
+  display: flex;
+  flex-direction: column-reverse;
+  pointer-events: none;
+  justify-content: center;
+  align-items: center;
+  gap: 16px;
   position: absolute;
   top: 0;
   left: 50%;
   translate: -50% 0;
+}
+
+wc-notification .wc-notification-item {
+  transition: all .3s;
+
+  pointer-events: auto;
+
+  max-width: 100vw;
   margin-top: 1rem;
   box-shadow: 0px 2px 5px #0003;
   padding: 1rem;
   opacity: 0;
   background-color:var(--wc-notification-bg, #000);
   color:var(--wc-notification-color, #eee);
-
+  height: min-content;
   transition: transform .5s ease;
   animation: fadeIn 0.5s ease forwards, fadeOut 0.5s ease 3s forwards;
 }
 
 @media screen and (max-width: 768px) {
-wc-notification .wc-notification-item {
-  font-size: .8rem
+  wc-notification .wc-notification-item {
+    font-size: .8rem
+  }
+
+  wc-notification .wc-notification-container {
+    gap: 12px
   }
 }
 
@@ -38,7 +54,8 @@ wc-notification .wc-notification-item {
 @keyframes fadeIn {
    to { opacity: 1; }
 }
-</style>`;
+</style>
+<div class="wc-notification-container"></div>`;
   }
 
   disconnectedCallback() {}
