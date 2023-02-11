944b6e579e8931ca06d5d4307b1c0e0bda074df7
genaroibc <bonavitaismael@gmail.com>
2023-02-11 18:43:41 -0300

    [FEAT]: let users customize styles and add responsive

--- wc-notification/wc-notification.js
+++ wc-notification/wc-notification.js
@@ -8,31 +8,37 @@ class WCNotification extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `<style>
 wc-notification .wc-notification-item {
+  max-width: 100vw;
   display: block;
   position: absolute;
   top: 0;
   left: 50%;
-  background-color: red;
   translate: -50% 0;
   margin-top: 1rem;
-  background-color: #000;
-  box-shadow: 0px 2px 5px 2px #0009;
+  box-shadow: 0px 2px 5px #0003;
   padding: 1rem;
   opacity: 0;
+  background-color:var(--wc-notification-bg, #000);
+  color:var(--wc-notification-color, #eee);
 
   transition: transform .5s ease;
   animation: fadeIn 0.5s ease forwards, fadeOut 0.5s ease 3s forwards;
 }
 
+@media screen and (max-width: 768px) {
+wc-notification .wc-notification-item {
+  font-size: .8rem
+  }
+}
+
 @keyframes fadeOut {
-   to { opacity: 0; color: red}
+   to { opacity: 0; }
 }
 
 @keyframes fadeIn {
    to { opacity: 1; }
 }
-</style>
-`;
+</style>`;
   }
 
   disconnectedCallback() {}
